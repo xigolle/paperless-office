d2d1556fba41cfa05152fc82cbabbf5d5ca190a3
xigolle <joey.driessen@live.nl>
2016-09-27 00:45:01 +0200

    Add first website design
    Website uses bootstrap, angular,jquery and has a sidebar hamburger menu style. And a search bar which scales when focussed.

---
+++ src/paperless-office-site/Scripts/AngularScript.js
@@ -0,0 +1,7 @@
+﻿//Here goes the code to developer with Angular
+var app = angular.module("app", []);
+
+app.controller("testCTRL", function ($scope) {
+    console.log("testing");
+    $scope.name = "joey";
+});
---
+++ src/paperless-office-site/Scripts/sidebar.js
@@ -0,0 +1,6 @@
+﻿$(function () {
+    $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+})
