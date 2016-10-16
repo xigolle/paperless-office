1395e9b8a78f064a8d8e11c820ff43de9bcdd019
root <root@Paperless-Office.h2etvqdod3dehe30cwn4jfim4a.ax.internal.cloudapp.net>
2016-10-16 19:09:51 +0000

    Make link to storage account and be able to download a certain or all files from a container to the server.

--- src/paperless-office-backend/backend/server.js
+++ src/paperless-office-backend/backend/server.js
@@ -1,12 +1,52 @@
 var express = require("express");
+var azure = require("azure-storage");
+var fs = require("fs");
+var config = require("../config.json");
 //var bodyparser = require("body-parser");
 var app = express();
+//Let's us work with containers and blobs
+var blobSvc = azure.createBlobService(config.storageAccountName, config.primaryKey);
 
-app.listen(3000,'13.94.234.60');
+
+app.listen(3000);
 
 app.get("/", function (req, res) {
     res.send("Hello world");
 });
 app.get("/api/getDocuments", function (req, res) {
-    res.send("Api to get documents");
+    
+    //This can be used to 'pipe' ONE document directly to the site    
+    //blobSvc.createReadStream("test", "Mathias/Knipsel.JPG").pipe(res);
+
+    //Gets all the document names that are in the specified container
+    blobSvc.listBlobsSegmented("test", null, function(error, result, response){
+  	if(!error){
+	    //Will download all the documents in the specified container
+	    result.entries.forEach(function(name) {
+        	getDoc("test", name.name);
+   	     
+	    });
+            
+      	    // result.entries contains the entries
+            // If not all blobs were returned, result.continuationToken has the continuation token. 
+            res.send(result.continuationToken);
+
+  	} else res.send("Could not get names");  
+    });
+
 });
+
+
+//Function that can be called to download a document to the server
+var getDoc = function(container, name) {
+    blobSvc.getBlobToStream(container, name, fs.createWriteStream(name), function(error, result, response){
+    	if(!error){
+	     console.log("blob retrieved");
+             //res.sendFile('/home/PaperlessOffice/node-server/output.pdf');
+        } else res.send("Could not retrieve file");
+    });
+}
+
+
+
+
