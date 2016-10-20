73e4e7961f1418338eb97ca02e038d52ed12d4fa
mathiassamyn <mathiassamyn@hotmail.com>
2016-10-20 16:03:18 +0200

    "finish" styling upload view

--- src/paperless-office-site/Scripts/uploadButton.js
+++ src/paperless-office-site/Scripts/uploadButton.js
@@ -2,6 +2,6 @@
     $("#upload").click(function (e) {
         e.preventDefault();
         $("#upload").toggleClass("uploadToggled");
-        $("#upload-zone-wrapper").toggleClass("collapse");
+        
     });
 })
