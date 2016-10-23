324ab9e9fd4f8f1b8ba230983805ed4e10c8b68b
mathiassamyn <mathiassamyn@hotmail.com>
2016-10-23 19:19:46 +0200

    Folder structure now same as github

--- src/paperless-office-backend/backend/server.js
+++ src/paperless-office-backend/backend/server.js
@@ -1,20 +1,30 @@
 var express = require("express");
 var azure = require("azure-storage");
 var fs = require("fs");
+var multer = require("multer");
 var config = require("../config.json");
 //var bodyparser = require("body-parser");
 var app = express();
 //Let's us work with containers and blobs
 var blobSvc = azure.createBlobService(config.storageAccountName, config.primaryKey);
 
+var testArray = [];
 
 app.listen(3000);
 
-app.get("/", function (req, res) {
-    res.send("Hello world");
+
+//This will show the website when a person visits 13.94.234.60:3000
+app.use(express.static("../../paperless-office-site"));
+
+app.get("/api/getDocument", function (req, res) {
+    //console.log(req.params.name);
+    console.log(req.get('test'));
+    //blobSvc.createReadStream("test", req.params.name).pipe(res)
+
 });
 app.get("/api/getDocuments", function (req, res) {
-    
+    //empty array
+    testArray = [];
     //This can be used to 'pipe' ONE document directly to the site    
     //blobSvc.createReadStream("test", "Mathias/Knipsel.JPG").pipe(res);
 
@@ -23,32 +33,66 @@ app.get("/api/getDocuments", function (req, res) {
   	if(!error){
 	    //Will download all the documents in the specified container
 	    result.entries.forEach(function(name) {
-        	getDoc("test", name.name);
-   	     
+	        getDoc("test", name.name);
+	        testArray.push(name.name);
+            
+   	        
 	    });
             
       	    // result.entries contains the entries
-            // If not all blobs were returned, result.continuationToken has the continuation token. 
-            res.send(result.continuationToken);
+  	    // If not all blobs were returned, result.continuationToken has the continuation token. 
+	    res.send(testArray);
+            //res.send(result.continuationToken);
 
   	} else res.send("Could not get names");  
     });
 
 });
 
-app.get("/api/uploadDocuments", function(req, res) {
-    //werken met block blobs, append blobs of page blobs?
-    //blob maken adhv een file die naar de server wordt gestuurd en er dus opstaat
-    //of werken met write stream?
+//Tries to make a user folder, and catches the error if it already exists. Bad code --> needs to be fixed: empty catch.
+var mkdirSync = function (path) {
+    try {
+        fs.mkdirSync(path);
+    } catch (e) {
+        //if (e.code != 'EEXIST') console.log( e);
+    }
+}
 
-    //local file
-    blobSvc.createBlockBlobFromLocalFile("test", "test.txt", "test.txt", function(error, result, response) {
-	if(!error) {
-	    res.send("file uploaded");
-        };
-    });
+//This will define the full storage path for the uploaded files.
+var storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        mkdirSync("./users/" + req.body.user);     
+        callback(null, "./users/"+req.body.user);
+    },
+    filename: function (req, file, callback) {
+        callback(null, file.originalname)
+    }
+});
 
-    //writeStream
+var upload = multer({ storage: storage }).any("myFile");
+
+app.post("/api/uploadDocuments", function (req, res) {
+    upload(req, res, function (err) {
+        if (err) {
+            console.log("Error Occured: " + err);
+            return;
+        }      
+        var userFolder = "./users/" + req.body.user
+
+        //voor demo: documenten worden ineens naar storage gestuurd
+        fs.readdir( userFolder, function( err, files ) {
+            files.forEach(function (file, index) {
+                blobSvc.createBlockBlobFromLocalFile("test", file, userFolder + "/" + file, function (error, result, response) {
+                    if (!error) {
+                        console.log("success");
+                        fs.unlinkSync(userFolder + "/" + file);
+                    } else console.log(error);
+                });
+            });
+        });      
+
+        res.end();
+    })
 });
 
 //Function that can be called to download a document to the server
