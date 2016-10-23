43eeae9b7f03f3315e9f487857a3148c0152cacb
mathiassamyn <mathiassamyn@hotmail.com>
2016-10-23 21:41:45 +0200

    fix no uploading bug

--- src/paperless-office-site/Scripts/AngularScript.js
+++ src/paperless-office-site/Scripts/AngularScript.js
@@ -57,44 +57,44 @@ app.controller("testCTRL", function ($scope, DocumentService) {
         });
 
     }
+});
+
+
+app.controller("uploadController", function ($scope, $http) {
 
 
 
-    app.controller("uploadController", function ($scope, $http) {
+    $scope.upload = function () {
 
-
-
-        $scope.upload = function () {
-
-            if (userAdded) {
-                $http.post("/api/uploadDocuments", fd, {
-                    withCredentials: true,
-                    headers: { 'Content-Type': undefined },
-                    transformRequest: angular.identity
-                }).then(function successCallback(response) {
-                    console.log("success");
-                }, function errorCallback(response) {
-                    console.log("failure");
-                });
-
-                fd = new FormData();
-                userAdded = false;
-            }
-        }
-
-        $scope.addFile = function (files) {
-
-            if (!userAdded) {
-                //Met deze lijn kunnen we de user meegeven
-                fd.append("user", "Mathias Samyn");
-                userAdded = true;
-            }
-            angular.forEach(files, function (file) {
-                fd.append("file", file);
+        if (userAdded) {
+            $http.post("/api/uploadDocuments", fd, {
+                withCredentials: true,
+                headers: { 'Content-Type': undefined },
+                transformRequest: angular.identity
+            }).then(function successCallback(response) {
+                console.log("success");
+            }, function errorCallback(response) {
+                console.log("failure");
             });
 
+            fd = new FormData();
+            userAdded = false;
         }
+    }
 
-    });
+    $scope.addFile = function (files) {
+
+        if (!userAdded) {
+            //Met deze lijn kunnen we de user meegeven
+            fd.append("user", "Mathias Samyn");
+            userAdded = true;
+        }
+        angular.forEach(files, function (file) {
+            fd.append("file", file);
+        });
+
+    }
+
 });
+
 
