7287ee5d3c56336dba025b4193d74412210add1f
mathiassamyn <mathiassamyn@hotmail.com>
2016-11-30 00:19:06 +0100

    try login

--- src/paperless-office-backend/backend/server.js
+++ src/paperless-office-backend/backend/server.js
@@ -236,5 +236,77 @@ var getDoc = function(container, name) {
 }
 
 
+//---------------------------------
+
+
+// dependencies
+var debug = require('debug')('passport-mongo');
+
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressSession = require('express-session');
+var mongoose = require('mongoose');
+var hash = require('bcrypt-nodejs');
+var path = require('path');
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
+
+// mongoose
+mongoose.connect('mongodb://localhost/mean-auth');
+
+// user schema/model
+var User = require('./models/user.js');
+
+// create instance of express
+var app = express();
+
+// require routes
+var routes = require('./routes/api.js');
+
+// define middleware
+app.use(express.static(path.join(__dirname, '../../paperless-office-site')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// configure passport
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// routes
+debugger;
+app.use('/user/', routes);
+
+/*app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client', 'index.html'));
+});*/
+
+// error handlers
+app.use(function (req, res, next) {
+    debugger;
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(function (err, req, res) {
+    res.status(err.status || 500);
+    res.end(JSON.stringify({
+        message: err.message,
+        error: {}
+    }));
+});
+
 
 
--- src/paperless-office-site/Scripts/AngularScript.js
+++ src/paperless-office-site/Scripts/AngularScript.js
@@ -1,5 +1,5 @@
 ﻿
-var app = angular.module("app", []);
+var app = angular.module("app", ["ngRoute"]);
 
 app.service('DocumentService', function ($http) {
 
@@ -157,4 +157,53 @@ app.controller("uploadController", function ($scope, $http) {
 
 });
 
+
+
+//--------------------------------
+
+app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+          templateUrl: 'partials/home.html',
+          access: { restricted: true }
+      })
+      .when('/login', {
+          templateUrl: 'partials/login.html',
+          controller: 'loginController',
+          access: { restricted: false }
+      })
+      .when('/logout', {
+          controller: 'logoutController',
+          access: { restricted: true }
+      })
+      .when('/register', {
+          templateUrl: 'partials/register.html',
+          controller: 'registerController',
+          access: { restricted: false }
+      })
+      .when('/one', {
+          template: '<h1>This is page one!</h1>',
+          access: { restricted: true }
+      })
+      .when('/two', {
+          template: '<h1>This is page two!</h1>',
+          access: { restricted: false }
+      })
+      .otherwise({
+          redirectTo: '/'
+      });
+});
+
+app.run(function ($rootScope, $location, $route, AuthService) {
+    $rootScope.$on('$routeChangeStart',
+      function (event, next, current) {
+          AuthService.getUserStatus()
+          .then(function () {
+              if (next.access.restricted && !AuthService.isLoggedIn()) {
+                  $location.path('/login');
+                  $route.reload();
+              }
+          });
+      });
+});
 
