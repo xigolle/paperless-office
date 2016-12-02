2f3bff8ce8058f1455378675432e539ef06d7ed9
mathiassamyn <mathiassamyn@hotmail.com>
2016-12-02 15:33:28 +0100

    dont knwo what happened

---
+++ src/paperless-office-site/Scripts/AngularScript.js
@@ -0,0 +1,201 @@
+﻿
+var app = angular.module("app", ["ngRoute"]);
+
+app.service('DocumentService', function ($http) {
+
+    this.items = [];
+    this.DocumentNames = [];
+
+    //this call doesn't get used at the moment will be maybe used in later us when we decide to use headers.
+    this.getDocument = function () {
+        console.log("get document function started");
+        $http({
+            method: 'GET',
+            url: "http://paperless-office.westeurope.cloudapp.azure.com/api/getDocument",
+            headers: {
+                "test": "Bedrijven.pdf"
+            }
+
+        }).then(function successCallback(response) {
+            console.log("succes" + response);
+        }, function errorCallback(response) {
+            console.log("error" + response);
+        });
+    }
+    this.getAmountDocuments = function () {
+        console.log("Get amount of documents and name");
+        return $http.get("http://paperless-office.westeurope.cloudapp.azure.com/api/getDocuments");
+
+    }
+})
+
+
+
+app.controller("testCTRL", function ($scope, DocumentService) {
+
+
+
+    //start function to show thumbnails of documents
+    //makes sure it runs when the website starts
+    showThumbnailOfDocuments();
+
+
+    function showThumbnailOfDocuments() {
+        var documentCallback = DocumentService.getAmountDocuments();
+        documentCallback.then(function (documentNames) {
+            ////after the list of documents is collected start getting documents
+            function sortNumber(a, b) {
+                return b.date - a.date;
+            }
+            var test = Date.parse(documentNames.data[0].date);
+            for (var i = 0; i < documentNames.data.length; i++) {
+                documentNames.data[i].date = Date.parse(documentNames.data[i].date);
+
+            }
+          
+
+            documentNames.data.sort(sortNumber);
+            for (var i = 0; i < documentNames.data.length; i++) {
+
+                
+                var newDocumentHolder = document.createElement('div');
+
+                var documentCanvas = document.createElement('canvas');
+                var documentIdentifier = document.createElement('span');
+
+                var documentIdentifierText = document.createTextNode(decodeURI(documentNames.data[i].name));
+                documentIdentifier.className = "document-identifier";
+                documentIdentifier.appendChild(documentIdentifierText);
+
+                newDocumentHolder.className = "Canvas-Document ";
+                documentCanvas.width = 306;
+                documentCanvas.height = 396;
+                documentCanvas.id = "canvass"+i;
+                var PDFWrapper = document.getElementById("Canvas-Document-Holder");
+                
+                newDocumentHolder.appendChild(documentIdentifier);
+                newDocumentHolder.appendChild(documentCanvas);
+                PDFWrapper.appendChild(newDocumentHolder);
+                var URLReadyDocument = encodeURI(documentNames.data[i].name);
+                
+
+                showMultiplePDFDocument("http://paperless-office.westeurope.cloudapp.azure.com/api/getDocumentURL/" + URLReadyDocument, "canvass" + i, URLReadyDocument);
+            }
+        });
+
+    }
+});
+
+
+app.controller("uploadController", function ($scope, $http) {
+
+    var fd = new FormData();
+    var userAdded = false; //TODO: rename var to resemble tha fact that at least 1 file has been added
+    var docNameAdded = false;
+
+    $scope.collapseDetails = "collapse";
+    $scope.collapseZone = "";
+    $scope.docName = "";
+    $scope.docLabels = "";
+
+
+    $scope.upload = function () {
+
+        if (userAdded) {
+            $scope.collapseDetails = "";
+            $scope.collapseZone = "collapse";
+            console.log($scope.docName);
+            if ($scope.docName.split(' ').join('') != "") {
+                docNameAdded = true;
+                fd.append("docName", $scope.docName);
+                fd.append("docLabels", $scope.docLabels);
+                $scope.docName = "";
+                console.log(fd);
+            }
+            if (docNameAdded) {
+                $http.post("/api/uploadDocuments", fd, {
+                    //withCredentials: true,
+                    headers: { 'Content-Type': undefined },
+                    transformRequest: angular.identity
+                }).then(function successCallback(response) {
+                    console.log("success");                 
+                }, function errorCallback(response) {
+                    console.log("failure");
+                });
+
+                $scope.collapseDetails = "collapse";
+                $scope.collapseZone = "";
+
+                fd = new FormData();
+                userAdded = false;
+                docNameAdded = false;
+            } else {
+                $scope.myStyle = { "border-color": "red" };
+            }
+            
+        } else {
+            $scope.myStyle = { "border-color": "darkred" };
+        }
+        
+    }
+
+    $scope.addFile = function (files) {
+        $scope.myStyle = { "border-color": "gray" };
+        console.log($scope.myStyle);
+        if (!userAdded) {
+            //Met deze lijn kunnen we de user meegeven
+            //fd.append("user", "mathiassamyn@hotmail.com");
+            userAdded = true;
+        }
+        angular.forEach(files, function (file) {
+            console.log("logging result of angularScript");
+            console.log(file);
+            fd.append("file", file);
+        });
+
+    }
+
+});
+
+
+
+//--------------------------------
+
+app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+          templateUrl: 'partials/home.html',
+          access: { restricted: true }
+      })
+      .when('/login', {
+          templateUrl: 'partials/login.html',
+          controller: 'loginController',
+          access: { restricted: false }
+      })
+      .when('/logout', {
+          controller: 'logoutController',
+          access: { restricted: true }
+      })
+      .when('/register', {
+          templateUrl: 'partials/register.html',
+          controller: 'registerController',
+          access: { restricted: false }
+      })
+      .otherwise({
+          redirectTo: '/'
+      });
+});
+
+app.run(function ($rootScope, $location, $route, AuthService) {
+    $rootScope.$on('$routeChangeStart',
+      function (event, next, current) {
+          AuthService.getUserStatus()
+          .then(function () {
+              if (next.access.restricted && !AuthService.isLoggedIn()) {
+                  $location.path('/login');
+                  $route.reload();
+              }
+          });
+      });
+});
+
---
+++ src/paperless-office-site/Scripts/PDFViewer.js
@@ -0,0 +1,211 @@
+﻿// URL of PDF document
+//needs to be on own server else you get cross server exception
+$(function () {
+    //showThumbnailOfDocuments(5);
+    $("#HomeButton").click(function () {
+        openListOfDocuments();
+    })
+
+    $("#loaddocument").click(function () {
+        openSinglePDFDocument("http://localhost:7152/Files/test0.pdf");
+    });
+    $("#DocumentIFrame").toggle();
+    $("#SuggestedDocumentSection").toggle();
+    $("#PDFDocumentWrapper").toggleClass("col-md-10");
+    $("#liquid").liquidcarousel({
+        height: 140,
+        hidearrows: false
+    });
+
+
+
+    var number = 0;
+    if (!PDFJS.workerSrc && typeof document !== 'undefined') {
+        // workerSrc is not set -- using last script url to define default location
+
+        PDFJS.workerSrc = (function () {
+            'use strict';
+            var scriptTagContainer = document.body ||
+                                     document.getElementsByTagName('head')[0];
+            var pdfjsSrc = scriptTagContainer.lastChild.src;
+            return pdfjsSrc && pdfjsSrc.replace(/\.js$/i, '.worker.js');
+        })();
+
+        PDFJS.workerSrc = 'pdfjs-dist-master/build/pdf.worker.js';
+    }
+    function createImageWrapper(src) {
+        var divWrapper = document.getElementById("previewDocuments");
+        console.log("Should have done something??");
+        var image = new Image();
+        image.src = src;
+        image.onload = function () {
+            var li = document.createElement('li');
+            var link = document.createElement('a');
+            link.appendChild(image);
+            li.appendChild(link);
+            $("#" + divWrapper.id).prepend(li);
+            //need this code to "reboot" the carousel to update with the new image
+            $("#liquid").liquidcarousel({
+                height: 140,
+                hidearrows: false
+            });
+        }
+    }
+    $("#inputUpload").change(function () {
+        console.log("We are changing stuff!");
+        console.log(this.files);
+        for (var i = 0; i < this.files.length; i++) {
+            if (this.files && this.files[i]) {
+                console.log("logging type");
+                console.log(this.files[i]["type"]);
+                //var thisCanvas = showImageInCanvas(i);
+                //console.log(thisCanvas.id);
+                switch (this.files[i]["type"]) {
+                    case "image/png":
+                    case "image/jpg":
+                    case "image/jpeg":
+                        if (this.files && this.files[i]) {
+                            var reader = new FileReader();
+                            reader.onload = function (e) {
+                                createImageWrapper(e.target.result);
+                                
+                            }
+
+                            reader.readAsDataURL(this.files[i]);
+                        }
+                        break;
+                    case "application/pdf":
+                        var reader = new FileReader();
+                        reader.onload = function (e) {
+                            console.log("Should show in a canvas!");
+                            showInCanvas(e.target.result);
+                        }
+                        reader.readAsDataURL(this.files[i]);
+                        break;
+                    default:
+                        console.log("sorry we do not support this file format!");
+                        break;
+                }
+
+            }
+        }
+
+    });
+
+    function convertDataURIToBinary(dataURI) {
+        var BASE64_MARKER = ';base64,';
+        var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+        var base64 = dataURI.substring(base64Index);
+        var raw = window.atob(base64);
+        var rawLength = raw.length;
+        var array = new Uint8Array(new ArrayBuffer(rawLength));
+
+        for (i = 0; i < rawLength; i++) {
+            array[i] = raw.charCodeAt(i);
+        }
+        return array;
+    }
+
+    function showInCanvas(url) {
+        // See README for overview
+        'use strict';
+        // Fetch the PDF document from the URL using promises
+        var pdfAsArray = convertDataURIToBinary(url);
+        console.log("trollings");
+        PDFJS.getDocument(pdfAsArray).then(function (pdf) {
+            // Using promise to fetch the page
+            pdf.getPage(1).then(function (page) {
+                var scale = 0.5;
+                var viewport = page.getViewport(scale);
+                // Prepare canvas using PDF page dimensions
+                //var canvas = document.getElementById('the-canvas');
+                var newCanvas = document.createElement('canvas');
+                newCanvas.id = "Canvas" + number;
+
+                //var divWrapper = document.getElementById("previewDocuments");
+                //console.log("Should have done something??");
+                //divWrapper.appendChild(newCanvas);
+                //canvas = document.getElementById("Canvas" + number);
+                number++;
+                var context = newCanvas.getContext('2d');
+                newCanvas.height = 306;
+                newCanvas.width = 396;
+                // Render PDF page into canvas context
+                var renderContext = {
+                    canvasContext: context,
+                    viewport: viewport
+                };
+                var task = page.render(renderContext);
+                task.promise.then(function () {
+                    
+                    createImageWrapper(newCanvas.toDataURL());
+
+                    
+
+                })
+            });
+        });
+    }
+});
+function showMultiplePDFDocument(url, canvasID, currentDoc) {
+    var url = url;
+    // Asynchronous download PDF
+    PDFJS.getDocument(url)
+      .then(function (pdf) {
+          return pdf.getPage(1);
+      })
+      .then(function (page) {
+          // Set scale (zoom) level
+          var scale = 0.5;
+
+          // Get viewport (dimensions)
+          var viewport = page.getViewport(scale);
+          var canvas = document.getElementById(canvasID);
+
+          // Fetch canvas' 2d context
+          var context = canvas.getContext('2d');
+
+          // Set dimensions to Canvas
+          canvas.height = viewport.height;
+          canvas.width = viewport.width;
+          canvas.width = 306;
+          canvas.height = 396;
+
+          // Prepare object needed by render method
+          var renderContext = {
+              canvasContext: context,
+              viewport: viewport
+          };
+
+          //set on click listener
+          //$("#" + newCanvas.id).data("foo", 52);
+          $("#" + canvasID).parent().data("currentDoc", currentDoc).click(function () {
+
+              var SingleDocumentURL = encodeURIComponent("http://paperless-office.westeurope.cloudapp.azure.com/api/getDocumentURL/" + $(this).data("currentDoc"));
+              openSinglePDFReader(SingleDocumentURL);
+          });
+
+          // Render PDF page
+          page.render(renderContext);
+      });
+}
+
+
+function openListOfDocuments() {
+    $("#Canvas-Document-Holder").toggle();
+    $("#DocumentIFrame").toggle();
+    $("#SuggestedDocumentSection").toggle();
+    $("#PDFDocumentWrapper").toggleClass("col-md-10");
+
+}
+function openSinglePDFReader(url) {
+    $("#Canvas-Document-Holder").toggle();
+    //$("#DocumentIFrame")
+    $("#PDFDocumentWrapper").toggleClass("col-md-10");
+
+    $("#SuggestedDocumentSection").toggle();
+    $("#DocumentIFrame").attr('src', "/web/viewer.html?file=" + url).toggle();
+
+}
+
+
